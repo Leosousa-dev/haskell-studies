3e29851cf3d8eea2297e1435e11874f845502d6f
leonardo <leonardocasa790@gmail.com>
2024-01-15 23:20:45 -0300

    update cabal and ghc version

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,4 @@
--- app/Main.hs
 module Main where
 
--- import module
-import Lib (sum',mul, sub , divd)
-
 main :: IO ()
-main = do
-    sum' 20 30
-    sub 6 2     
-    mul  10 3
-    divd 6 3
+main = putStrLn "Hello, Haskell!"
