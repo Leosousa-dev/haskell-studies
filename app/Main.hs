71b5b4c84ffbde34b6be8e35ee5917332fa1753b
leonardo <leonardocasa790@gmail.com>
2024-01-16 22:59:26 -0300

    chore: learning functions with types

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,18 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "hello world"
+main = putStrLn body_
 
 
 title_ :: String -> String
 title_  content  = "<title>" <> content <> "</title>"
 
+html_ :: String -> String
+html_ = el "html"
 
+body_ :: String -> String
+body_ = el "body"
+
+el :: String -> String -> String
+el tag content =
+    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
