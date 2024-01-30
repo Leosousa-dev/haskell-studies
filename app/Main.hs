3377795105f9f8b53471712a174a03202bfef162
leonardo <leonardocasa790@gmail.com>
2024-01-29 23:53:04 -0300

    chore(control): using guards

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,9 @@
 module Main where
 
 -- import Lambda.Functions (biggerNum)
-import ControlStructure.Control (oddOrEven)
+import ControlStructure.Control (oddOrEven, checkvalue)
 
 main :: IO ()
 main = do 
     -- print(sumLamb 4 9)
-    print $ oddOrEven 31
+    print $ checkvalue 0
--- src/ControlStructure/Control.hs
+++ src/ControlStructure/Control.hs
@@ -1,9 +1,16 @@
-module ControlStructure.Control (oddOrEven) where
+module ControlStructure.Control (oddOrEven , checkvalue) where
 
 
-    --if then else
+    -- if then else
     oddOrEven :: Int -> String
     oddOrEven n =
         if even n
         then "number is Even."
         else "number is Odd."
+
+    -- Guards
+    checkvalue :: Int -> String
+    checkvalue x  
+        | x < 0     = "number is negative"
+        | x == 0    = "Zero" 
+        | otherwise = "number is positive"
