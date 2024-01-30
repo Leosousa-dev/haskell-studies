8dbf04f7fe26e9966d3ec7a769914193cbcc67bf
leonardo <leonardocasa790@gmail.com>
2024-01-29 23:54:50 -0300

    chore(control): create cases

--- src/ControlStructure/Control.hs
+++ src/ControlStructure/Control.hs
@@ -14,3 +14,16 @@ module ControlStructure.Control (oddOrEven , checkvalue) where
         | x < 0     = "number is negative"
         | x == 0    = "Zero" 
         | otherwise = "number is positive"
+    
+    -- case
+    messageDayWeek :: Int -> String
+    messageDayWeek day =
+        case day of
+            1 -> "Monday"
+            2 -> "Tuesday"
+            3 -> "Wednesday"
+            4 -> "Thursday"
+            5 -> "Friday"
+            6 -> "Saturday"
+            7 -> "Sunday"
+            _ -> "Invalid day"
