56f1900bec8f39bbae9b605a05cbea1d08fb2d78
leonardo <leonardocasa790@gmail.com>
2024-01-29 23:45:34 -0300

    chore(control):  studying control flow

---
+++ src/ControlStructure/Control.hs
@@ -0,0 +1,9 @@
+module ControlStructure.Control () where
+
+
+    --if then else
+    oddOrEven :: Int -> String
+    oddOrEven n =
+        if even n
+        then "number is Even."
+        else "number is Odd."
